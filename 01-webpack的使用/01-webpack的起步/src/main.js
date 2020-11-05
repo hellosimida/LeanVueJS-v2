4fb1fb62cd91dafc82f42f7620eb530e30b242fc
hellosimida <842214471@qq.com>
2020-11-05 14:52:12 +0800

    webpack学习

---
+++ 01-webpack的使用/01-webpack的起步/src/main.js
@@ -0,0 +1,12 @@
+//使用commonjs的模块化规范
+const {add,mul} = require('./mathUtil.js');
+
+console.log(add(1,2));
+console.log(mul(2,3));
+
+//使用es6的模块化规范
+import {name,age,height} from "./info"
+
+console.log(name);
+console.log(age);
+console.log(height);
