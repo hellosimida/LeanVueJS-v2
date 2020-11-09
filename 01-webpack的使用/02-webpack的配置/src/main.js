853fa2e8d11e20dfe8a6f6a738cee8c3ad95f342
hellosimida <842214471@qq.com>
2020-11-09 15:37:16 +0800

    webpack学习

--- 01-webpack的使用/02-webpack的配置/src/main.js
+++ 01-webpack的使用/02-webpack的配置/src/main.js
@@ -1,11 +1,11 @@
 //使用commonjs的模块化规范
-const {add,mul} = require('./mathUtil.js');
+const {add,mul} = require('./js/mathUtil.js');
 
 console.log(add(1,2));
 console.log(mul(2,3));
 
 //使用es6的模块化规范
-import {name,age,height} from "./info"
+import {name,age,height} from "./js/info"
 
 console.log(name);
 console.log(age);
